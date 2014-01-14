ff86ea8dc324544c7d0e62cec53c146d9fa8ef8f
wcjohnson11 <williamcouperjohnson@gmail.com>
2014-01-14 11:56:25 -0500

    Disabled twit.js on server side because of app crash

---
+++ server/twit.js
@@ -0,0 +1,15 @@
+/*var T = new Twit({
+  consumer_key:         '<your keys here>',
+  consumer_secret:      '<your keys here>',
+  access_token:         '<your keys here>',
+  access_token_secret:  '<your keys here>',
+});
+
+var newYork = [ '-74.253595', '40.49704', '-73.700272', '40.932895' ];
+
+var stream = T.stream('statuses/filter', { locations: newYork });
+
+stream.on('tweet', function (tweet) {
+  console.log(tweet);
+});
+*/
