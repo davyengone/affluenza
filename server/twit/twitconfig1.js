171afd96e78294dd5c3e4a5bfccaeaffb0a3e286
wcjohnson11 <williamcouperjohnson@gmail.com>
2014-01-15 14:09:10 -0500

    Updated twitter configs
    Connect to twit streaming API and update all tweets that have NY geo
    data

--- server/twit/twitconfig1.js
+++ server/twit/twitconfig1.js
@@ -1,15 +1,19 @@
-/*var T = new Twit({
-  consumer_key:         '<your keys here>',
-  consumer_secret:      '<your keys here>',
-  access_token:         '<your keys here>',
-  access_token_secret:  '<your keys here>',
-});
+locstream = new Metoer.Stream('loc');
 
-var newYork = [ '-74.253595', '40.49704', '-73.700272', '40.932895' ];
-
-var stream = T.stream('statuses/filter', { locations: newYork });
-
-stream.on('tweet', function (tweet) {
-  console.log(tweet);
-});
-*/
+if (Meteor.isServer) {
+	Meteor.startup(function () {
+			var T = new Twit({
+				consumer_key:         '<your keys here>',
+				consumer_secret:      '<your keys here>',
+				access_token:         '<your keys here>',
+				access_token_secret:  '<your keys here>'
+			});
+			var newYork = [ '-74.253595', '40.49704', '-73.700272', '40.932895' ];
+			var stream = T.stream('statuses/sample', { locations: newYork });
+			stream.on('data', function (data) {
+				if (data.geo){
+					locstream.emit('update', {coords: data.geo.coordinates, lang: data.lang, msg:data.text});
+				}
+			});
+	});
+}
