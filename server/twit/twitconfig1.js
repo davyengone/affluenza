785100fd3e384a9b7c108b9ab269af58c43d3831
wcjohnson11 <williamcouperjohnson@gmail.com>
2014-01-15 14:30:37 -0500

    Added more code to twit.config, still disabled
    twit is being a jerk, breaking the app

--- server/twit/twitconfig1.js
+++ server/twit/twitconfig1.js
@@ -1,19 +1,28 @@
-locstream = new Metoer.Stream('loc');
+/*
+locstream = new Meteor.Stream('loc');
 
 if (Meteor.isServer) {
-	Meteor.startup(function () {
-			var T = new Twit({
-				consumer_key:         '<your keys here>',
-				consumer_secret:      '<your keys here>',
-				access_token:         '<your keys here>',
-				access_token_secret:  '<your keys here>'
+	Meteor.methods({
+		twit_get: function() {
+			var T = new TwitMaker({
+				consumer_key:         'foo',
+				consumer_secret:      'foo',
+				access_token:         'foo',
+				access_token_secret:  'foo'
 			});
 			var newYork = [ '-74.253595', '40.49704', '-73.700272', '40.932895' ];
-			var stream = T.stream('statuses/sample', { locations: newYork });
+			/*var stream = T.stream('statuses/sample', { locations: newYork });
 			stream.on('data', function (data) {
 				if (data.geo){
 					locstream.emit('update', {coords: data.geo.coordinates, lang: data.lang, msg:data.text});
 				}
-			});
+			});*//*
+			Twit.get(
+				'search/tweets', { locations: newYork},
+				function(err, reply) {
+					console.log(reply);
+				});
+		}
 	});
 }
+*/
